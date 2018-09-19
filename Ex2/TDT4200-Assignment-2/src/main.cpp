6f66c674cc12814b4c4b292fe6932e7c99e5abef
RonaldHauw <ronald.hauwaerts@gmail.com>
2018-09-19 17:22:59 +0200

    push it

--- Ex2/TDT4200-Assignment-2/src/main.cpp
+++ Ex2/TDT4200-Assignment-2/src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include "utilities/OBJLoader.hpp"
 #include "utilities/lodepng.h"
 #include "rasteriser.hpp"
+#include <mpi.h>
 
 int main(int argc, char **argv) {
 	std::string input("../input/sphere.obj");
@@ -10,6 +12,17 @@ int main(int argc, char **argv) {
 	unsigned int width = 1920;
 	unsigned int height = 1080;
 	unsigned int depth = 3;
+    int scale;
+
+    int comm_sz; // size of the communicator
+    int my_rank; // rank of the individual process
+
+    MPI_Init(NULL, NULL); // ronald: setting up MPI environment
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); // getting the size of the communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // getting the individual rank
+
+    std::cout << "Hi there! I'm process number " << my_rank << " of the "<<comm_sz<<"!"<<std::endl;
 
 	for (int i = 1; i < argc; i++) {
 		if (i < argc -1) {
@@ -26,11 +39,39 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
+	std::string my_rank_string = std::to_string(my_rank);
+    output = "output/file_nb_" + my_rank_string +".png";
+	if(my_rank == 0) {
+		for(int i = 1; i < comm_sz; ++i) {
+			scale = i+1; // just declare something different for all processess
+			MPI_Send(
+					&scale, // pointer to stored data
+					1, // packet size
+					MPI_INT, // packet type
+					i, // receiver
+					0, // tag
+					MPI_Comm MPI_COMM_WORLD);
+            // communicator
+		}
+	}
+	else {
+		MPI_Recv(
+				&scale,
+				1,
+				MPI_INT,
+				0,
+				0,
+				MPI_COMM_WORLD,
+				MPI_STATUS_IGNORE);
+	}
+
+
+
 	std::cout << "Loading '" << input << "' file... " << std::endl;
 
 	std::vector<Mesh> meshs = loadWavefront(input, false);
 
-	std::vector<unsigned char> frameBuffer = rasterise(meshs, width, height, depth);
+	std::vector<unsigned char> frameBuffer = rasterise(meshs, scale*width, scale*height, depth);
 
 	std::cout << "Writing image to '" << output << "'..." << std::endl;
 
@@ -40,6 +81,6 @@ int main(int argc, char **argv) {
 	{
 		std::cout << "An error occurred while writing the image file: " << error << ": " << lodepng_error_text(error) << std::endl;
 	}
-
+    MPI_Finalize();
 	return 0;
 }
